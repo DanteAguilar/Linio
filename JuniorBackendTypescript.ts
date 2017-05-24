25a0d41e19f2d9073cc4564eacf571c4de6ab5ef
DanteAguilar <phxdansuke@gmail.com>
2017-05-24 12:36:35 -0400

    Create JuniorBackendTypescript.ts

---
+++ JuniorBackendTypescript.ts
@@ -0,0 +1,9 @@
+var x:number = 1;
+
+while(x>=100) { 
+   if (x%3==0 && x%5==0) {console.log("Linianos");}
+    else if (x%3==0) {console.log("Linio");}
+    else if (x%5==0) {console.log("IT")}
+    else console.log(x);
+    x++;
+} 
