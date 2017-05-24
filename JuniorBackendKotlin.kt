0c8fa5c08c9554814a820a614af0830dd05031c8
DanteAguilar <phxdansuke@gmail.com>
2017-05-23 21:52:53 -0400

    Create JuniorBackendKotlin.kt

---
+++ JuniorBackendKotlin.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+   var x = 1;
+   while(x<=100)
+    {
+      if(x%3==0 && x%5==0){ println("Linianos")}
+        else if (x%3==0) {println("Linio")}
+        else if (x%5==0) {println("IT")}
+        else println(x)
+        x++;
+    }
